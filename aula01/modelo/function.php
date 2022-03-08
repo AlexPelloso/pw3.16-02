e29dacecabe8c80a4e283e7aac291d2d053f3683
AlexPelloso <alex.pelloso@etec.sp.gov.br>
2022-03-08 11:04:11 -0300

    Commit -- atualizando

--- aula01/modelo/function.php
+++ aula01/modelo/function.php
@@ -1,31 +1,33 @@
-<?php 
+<?php
 
-    $NOME = $_REQUEST['NOME'];
-    $ANIMAL = $_REQUEST['ANIMAL'];
+$NOME = $_REQUEST['NOME'];
+$ANIMAL = $_REQUEST['ANIMAL'];
 
-    if(empty($NOME)){
-        $dados = array(
-            "tipo" => 'error',
-            "mensagem" => 'Existe(m) campo(s) a ser(em) preenchido(s).'
+if(empty($NOME)){
+    $dados = array(
+        "tipo" => 'error',
+        "mensagem" => 'Existe(m) campo(s) a ser(em) prenchido(s).'
+    );
+}else{
+
+    switch($ANIMAL){
+        case '1' : $dados = array(
+            "tipo" => 'cachorro.jpg',
+            "mensagem" => 'Seja bem vindo, '.$NOME. ', sabemos que seu animal preferido é o cachorro'   
         );
-    }else{
-        switch($ANIMAL){
-            case '1' : $dados = array(
-                "tipo" => 'cachorro',
-                "mensagem" => 'Seja bem vindo, '.$NOME.', sabemos que o seu animal de preferência é cachorro'
-            );
-            break;
-            case '2' : $dados = array(
-                "tipo" => 'gato',
-                "mensagem" => 'Seja bem vindo, '.$NOME.', sabemos que o seu animal de preferência é gato'
-            );
-            break;
-            case '3' : $dados = array(
-                "tipo" => 'peixe',
-                "mensagem" => 'Seja bem vindo, '.$NOME.', sabemos que o seu animal de preferência é peixe'
-            );
-            break;
-        }
+        break;
+        case '2' : $dados = array(
+            "tipo" => 'gato.jpg',
+            "mensagem" => 'Seja bem vindo, '.$NOME. ', sabemos que seu animal preferido é o gato'   
+        );
+        break;
+        case '3' : $dados = array(
+            "tipo" => 'peixe.jpg',
+            "mensagem" => 'Seja bem vindo, '.$NOME. ', sabemos que seu animal preferido é o peixe'   
+        );
+        break;
     }
+
+}
 
 echo json_encode($dados);
