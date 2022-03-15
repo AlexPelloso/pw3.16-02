eb7ddbe504df5c21fccb3dd3884ad04920fb0d87
AlexPelloso <alex.pelloso@etec.sp.gov.br>
2022-03-15 11:26:28 -0300

    COmmit

--- aula02/controle/function.js
+++ aula02/controle/function.js
@@ -17,10 +17,12 @@ $(document).ready(function(){
             success: function(dados){
         
                 $('#retorno').append(`<div class="col-12 col-sm-8 col-md-6">
+                <div class="alert-primary">
                     <h1 class="text-white text-center">
                         ${dados.mensagem}
                     </h1>
-
+                    
+                </div>
             </div>`)
             }
         })
